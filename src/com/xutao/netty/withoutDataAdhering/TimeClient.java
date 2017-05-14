5f309ddadb0c2f07e8707eadabdaf2eae9269891
Tau Hsu <anexu1992@163.com>
2017-05-14 18:28:54 +0800

    TCP 解决粘包

---
+++ src/com/xutao/netty/withoutDataAdhering/TimeClient.java
@@ -0,0 +1,87 @@
+package com.xutao.netty.withoutDataAdhering;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by Tau Hsu on 2017/5/14.
+ */
+public class TimeClient {
+    private void connect(int port, String host) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class).option(ChannelOption.TCP_NODELAY, true).handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    System.out.println("client initChannel..");
+                    ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                    ch.pipeline().addLast(new StringDecoder());
+                    ch.pipeline().addLast(new TimeClientHandler());
+
+                }
+            });
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            future.channel().closeFuture().sync();
+
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]).intValue();
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("Input params must be String of Integer", e);
+            }
+        }
+
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+}
+
+class TimeClientHandler extends ChannelInboundHandlerAdapter{
+    private static final Logger LOGGER = Logger.getLogger(TimeClientHandler.class.getName());
+    private int counter;
+    private byte[] req;
+
+    public TimeClientHandler(){
+        req = ("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
+        System.out.println("rep length : "+ ("QUERY TIME ORDER" + System.getProperty("line.separator")).length());
+    }
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ByteBuf message = null;
+        for (int i = 0; i < 100; i++){
+            message = Unpooled.buffer(req.length);
+            message.writeBytes(req);
+            ctx.writeAndFlush(message);
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String resp = (String)msg;//只会收到去除掉“line.separator”字符串
+        System.out.println("resp length : " + resp.length());
+        System.out.println("Now is : " + resp + "; the counter is :" + ++counter);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        LOGGER.warning("Unexpected exception from downstram : " + cause.getMessage());
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/com/xutao/netty/withoutDataAdhering/TimeServer.java
@@ -0,0 +1,80 @@
+package com.xutao.netty.withoutDataAdhering;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.util.Date;
+
+/**
+ * Created by Tau Hsu on 2017/5/14.
+ */
+public class TimeServer {
+
+    private void bind(int port) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup,workGroup).channel(NioServerSocketChannel.class).
+                    option(ChannelOption.SO_BACKLOG,1024).
+                    childHandler(new ChildChannelHandler());
+            ChannelFuture future = bootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    class ChildChannelHandler extends ChannelInitializer<SocketChannel>{
+
+        @Override
+        protected void initChannel(SocketChannel ch) throws Exception {
+            System.out.println("server initChannel..");
+            ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+            ch.pipeline().addLast(new StringDecoder());
+            ch.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        int port = 8080;
+        if(args != null && args.length > 0){
+            try{
+                port = Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+                throw new RuntimeException("Input params must be String of Integer");
+            }
+        }
+        new TimeServer().bind(port);
+    }
+}
+
+class TimeServerHandler extends ChannelInboundHandlerAdapter{
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String)msg;//直接就可使用String
+        System.out.println("body length : " + body.length());
+        System.out.println("The time server recevie order : " + body);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)?new Date(System.currentTimeMillis()).toString() : "BAD ORDER";//会使用“line.separator”分割包
+        currentTime = currentTime + System.getProperty("line.separator");
+        System.out.println("currentTime length : " + currentTime.length());
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("server exceptionCaught..");
+        ctx.close();
+    }
+}
