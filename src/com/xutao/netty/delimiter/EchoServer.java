1a371a006035134cd2123c8c88f93caaf15a8877
Tau Hsu <anexu1992@163.com>
2017-05-20 11:07:27 +0800

    delimiter 编码器

---
+++ src/com/xutao/netty/delimiter/EchoServer.java
@@ -0,0 +1,79 @@
+package com.xutao.netty.delimiter;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Created by Tau Hsu on 2017/5/18.
+ */
+public class EchoServer {
+    public void bind(int port) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup,workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ByteBuf buf = Unpooled.copiedBuffer("$_".getBytes());
+                            ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,buf));
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        int port = 8080;
+        if(args != null && args.length > 0 ){
+            try{
+                port = Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+                throw new RuntimeException("inpurt params must be integer");
+            }
+        }
+        new EchoServer().bind(port);
+    }
+}
+
+class EchoServerHandler extends ChannelInboundHandlerAdapter{
+    int counter;
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String)msg;
+        System.out.println("This is " + ++counter + " this receive client : [" + body + "]");
+        body = body + "$_";
+        ByteBuf echo = Unpooled.copiedBuffer(body.getBytes());
+        ctx.writeAndFlush(echo);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("EchoServer read completed!");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
