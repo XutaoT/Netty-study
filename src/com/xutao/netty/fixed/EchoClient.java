19159e870599d87724acc68821876630358700de
Tau Hsu <anexu1992@163.com>
2017-05-20 22:01:06 +0800

    fixed 编码器

---
+++ src/com/xutao/netty/fixed/EchoClient.java
@@ -0,0 +1,7 @@
+package com.xutao.netty.fixed;
+
+/**
+ * Created by Tau Hsu on 2017/5/20.
+ */
+public class EchoClient {
+}
---
+++ src/com/xutao/netty/fixed/EchoServer.java
@@ -0,0 +1,72 @@
+package com.xutao.netty.fixed;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Created by Tau Hsu on 2017/5/20.
+ */
+public class EchoServer {
+
+    public void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workGroup).channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {//必需有childHandler，会报childHandler not set
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new FixedLengthFrameDecoder(20));
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        int port = 8080;
+        if(args != null && args.length > 0){
+            try{
+                port = Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+                throw new RuntimeException("the input params must be integer !", e);
+            }
+        }
+        new EchoServer().bind(port);
+    }
+}
+
+class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Receive client : [" + msg + "]");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Server readComplete !");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
